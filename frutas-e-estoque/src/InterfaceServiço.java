5527d2555e6523ec8b234d202bade953c4e7c3f5
guilherme ramon <70776815+guilhermeramon2@users.noreply.github.com>
2025-05-22 19:54:03 -0300

    Update InterfaceServiço.java

--- frutas-e-estoque/src/InterfaceServiço.java
+++ frutas-e-estoque/src/InterfaceServiço.java
@@ -2,5 +2,5 @@ interface FrutaServico {
     void cadastrarFruta(String nome, String cor, double preco, int estoque);
     void venderFruta(String nome, int quantidade);
     void listarFrutas();
-    void removerFruta(String nome);
+    void removerFruta(String nome);.
 }
